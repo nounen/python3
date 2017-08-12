69a7072eb354f5f20717b0773c0078611331694b
nounen <281580047@qq.com>
2017-08-13 00:46:45 +0800

    3.2.Python基础--字符串和编码 编码的演变; 字符 / 字符串, 以及字符串的编码和解码; 格式化(占位符).

---
+++ examples/3.4.string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print(ord('A'))
+# 65
+
+print(ord('中'))
+# 20013
+
+
+print(chr(65))
+# A
+
+print(chr(20013))
+# 中
+
+
+# 16 进制写 str
+print('\u4e2d\u6587')
+# 中文 (20013 转十六进制为 4e2d)
