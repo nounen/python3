05357ddfca23e8d4a6fcfb5132572e2dc44b4c97
nounen <281580047@qq.com>
2017-08-29 23:36:45 +0800

    私有属性

--- examples/8.0.Student.py
+++ examples/8.0.Student.py
@@ -3,9 +3,10 @@
 
 class Student(object):
 
-    def __init__(self, name, score):
+    def __init__(self, name, score, private_name="Pname"):
         self.name = name
         self.score = score
+        self.__private_name = private_name
 
     def print_score(self):
         print('%s: %s' % (self.name, self.score))
@@ -15,3 +16,6 @@ c = Student('Cast', 30)
 
 b.print_score()
 c.print_score()
+
+# print(b.__private_name)       # 不能直接访问私有属性, 会报错
+print(b._Student__private_name) # 访问私有属性的小技巧
