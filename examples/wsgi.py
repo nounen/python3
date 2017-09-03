35ed95e652cfe4e7ec44f3be1d0e58d3f44c9f2e
nounen <281580047@qq.com>
2017-09-03 19:21:44 +0800

    wsgi

---
+++ examples/wsgi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b'<h1>Hello, web!</h1>']
+
+# 从wsgiref模块导入:
+from wsgiref.simple_server import make_server
+
+# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+# 开始监听HTTP请求:
+httpd.serve_forever()
